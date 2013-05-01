e8b119c06bd1b31d6750895a78500bc7182e8a7a
Palsson123 <jacpa975@student.liu.se>
2013-05-01 21:37:01 +0200

    Changed the UPS-system so it doesn't get stuck in a loop. Added gravity to the Player-class. Made it possible to jump with the player.

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -1,21 +1,30 @@
 package game;
 
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Frame;
+import java.awt.DisplayMode;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+import javax.swing.JFrame;
 
 import player.Player;
 
-public class Game extends Frame implements Runnable, KeyListener {
+
+
+public class Game implements Runnable, KeyListener {
 	/**
 	 * Klassen som ritar ut allt och kör Game-loopen Senast uppdaterad av: Jacob
 	 * Pålsson Senast uppdaterad den: 4/30/2013
@@ -24,7 +33,9 @@ public class Game extends Frame implements Runnable, KeyListener {
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	int SCREENWIDTH = (int) screenSize.getWidth();
 	int SCREENHEIGHT = (int) screenSize.getHeight();
-	private Player player = new Player();
+	
+	Player player = new Player();
+	
 	public boolean running = false;
 
 	/**
@@ -32,31 +43,16 @@ public class Game extends Frame implements Runnable, KeyListener {
 	 */
 	public static void main(String[] args) {
 		new Game().start();
-
+		
 	}
-
+	JFrame app = new JFrame();
+	Canvas canvas = new Canvas();
+	int i=0;
 	public Game() {
 
-		this.addKeyListener(this); // Lägger till KeyListener så spelet kan
-									// hantera knapptryckningar
-
-		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment(); // Hämtar
-																						// grafiska
-																						// miljön
-		GraphicsDevice device = env.getDefaultScreenDevice(); //
-		GraphicsConfiguration gc = device.getDefaultConfiguration(); // Hämtar
-																		// grafikkortets
-																		// konfigurationer
-
-		this.setUndecorated(true); // Sätter undecorated så ingen ram finns på
-									// spelet, bara det som ritas syns.
-		this.setIgnoreRepaint(true); // I HAVE NO IDEA WHAT I'M DOING
-		device.setFullScreenWindow(this); // Sätter full screen mode, verkar
-											// sätta rätt upplösning direkt
-											// anpassat efter skärmen
 
 	}
-
+	
 	public synchronized void start() {
 		running = true;
 		new Thread(this).start();
@@ -74,79 +70,229 @@ public class Game extends Frame implements Runnable, KeyListener {
 	 * Nedan kommer en funktion som kör gameloopen, anpassat för 60 UPS
 	 */
 	public void run() {
-		long lastTime = System.nanoTime();
+
+	    // Create game window...
+	    JFrame app = new JFrame();
+	    int ups=0;
+	    app.setIgnoreRepaint( true );
+
+	    app.setUndecorated( true );
+
+	                
+
+	    // Add ESC listener to quit...
+
+	    app.addKeyListener( new KeyAdapter() {
+
+	      public void keyPressed( KeyEvent e ) {
+	    	  player.keyPressed(e);
+
+	        if( e.getKeyCode() == KeyEvent.VK_ESCAPE )
+
+	            running = false;
+
+	          }
+	      public void keyReleased( KeyEvent e )
+	      {
+	    	  player.keyReleased(e);
+	      }
+
+	    });
+
+	                
+
+	    // Get graphics configuration...
+
+	    GraphicsEnvironment ge = 
+
+	        GraphicsEnvironment.getLocalGraphicsEnvironment();
+
+	    GraphicsDevice gd = ge.getDefaultScreenDevice();
+
+	    GraphicsConfiguration gc = gd.getDefaultConfiguration();
+
+
+
+	    // Change to full screen
+
+	    gd.setFullScreenWindow( app );
+
+	    if( gd.isDisplayChangeSupported() ) {
+
+	      gd.setDisplayMode( 
+
+	        new DisplayMode( SCREENWIDTH, SCREENHEIGHT, 32, DisplayMode.REFRESH_RATE_UNKNOWN )
+
+	      );
+
+	    }
+
+	                
+
+	    // Create BackBuffer...
+
+	    app.createBufferStrategy( 2 );
+
+	    BufferStrategy buffer = app.getBufferStrategy();
+
+	                
+
+	    // Create off-screen drawing surface
+
+	    BufferedImage bi = gc.createCompatibleImage( SCREENWIDTH, SCREENHEIGHT );
+
+
+
+	    // Objects needed for rendering...
+
+	    Graphics graphics = null;
+
+	    Graphics2D g2d = null;
+
+	    Color background = Color.BLACK;
+
+	    Random rand = new Random();
+
+	                
+
+	    // Variables for counting frames per seconds
+
+	    int fps = 0;
+
+	    int frames = 0;
+
+	    long totalTime = 0;
+
+	    long curTime = System.currentTimeMillis();
+
+	    long lastTime = curTime;
+
+	                
+
+	    running = true;
+
+		long currentTime = System.currentTimeMillis();
 		double nsPerTick = 1000000000D / 60D;
-
-		int ups = 0;
-		int fps = 0;
-
-		long lastTimer = System.currentTimeMillis();
+		
 		double delta = 0;
 
-		while (running) {
-			long now = System.nanoTime();
-			delta += (now - lastTime) / nsPerTick;
-			lastTime = now;
-			boolean shouldRender = true;
-			while (delta >= 1) {
-				ups++;
-				update();
-				delta -= 1;
-				shouldRender = true;
-			}
+	    
+	    while( running ) {
+	    	
+	      try {
 
-			try {
-				Thread.sleep(2);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			if (shouldRender) {
-				fps++;
-				render();
-			}
+	        // count Frames per second...
 
-			if (System.currentTimeMillis() - lastTimer >= 1000) {
-				lastTimer += 1000;
-				System.out.println(ups + " ups, " + fps + " fps");
-				this.setTitle("ups: " + ups + ", fps: " + fps);
-				fps = 0;
-				ups = 0;
-			}
-		}
+	        lastTime = curTime;
 
+	        curTime = System.currentTimeMillis();
+
+	        totalTime += curTime - lastTime;
+
+	        if( totalTime > 1000 ) {
+
+	          totalTime -= 1000;
+
+	          fps = frames;
+
+	          frames = 0;
+
+	        } 
+
+	        ++frames;
+
+
+
+	        // clear back buffer...
+
+	        g2d = bi.createGraphics();
+
+	        g2d.setColor( background );
+
+	        g2d.fillRect( 0, 0, SCREENWIDTH, SCREENHEIGHT );
+
+	                                
+
+	        // draw some rectangles...
+
+	        
+	        player.paint(g2d);
+	                                
+
+	        // display frames per second...
+
+	        g2d.setFont( new Font( "Courier New", Font.PLAIN, 12 ) );
+
+	        g2d.setColor( Color.GREEN );
+
+	        g2d.drawString( String.format( "FPS: %s", fps ), 20, 20 );
+	        
+	        
+	        
+	        
+	    	if(ups <=60)
+	    	{
+	    		
+	    		update();
+	    		ups++;
+	    	}
+	    	if(System.currentTimeMillis() - currentTime > 1000)
+	    	{
+	    		currentTime = System.currentTimeMillis();
+	    		ups = 0;
+	    	}
+	    		
+	        // Blit image and flip...
+
+	        graphics = buffer.getDrawGraphics();
+
+	        graphics.drawImage( bi, 0, 0, null );
+
+	                                
+
+	        if( !buffer.contentsLost() )
+
+	          buffer.show();
+
+	        
+
+	      } finally {
+
+	        // release resources
+
+	        if( graphics != null ) 
+
+	          graphics.dispose();
+
+	        if( g2d != null ) 
+
+	          g2d.dispose();
+
+	      }
+
+	    }
+
+	                
+
+	    gd.setFullScreenWindow( null );
+
+	    System.exit(0);
 	}
 
 	public void update() {
 		player.move();
+        
 	}
-	BufferStrategy bs = getBufferStrategy();
+	
 	public void render() {
 		
-		if (bs == null) {
-			
-			this.createBufferStrategy(3);
-			bs = getBufferStrategy();
-			return;
-		}
-
-		Graphics g = bs.getDrawGraphics(); // Hämtar graphics-objekt från
-											// bufferstrategy
-		Graphics2D g2 = (Graphics2D) g; // Konverterar till Graphics2D utifall
-										// anti-aliasing vill användas
-		// Ritar ut en svart bakgrund på skärmen med samma storlek som skärmen
-		g2.setColor(Color.BLACK);
-		g2.fillRect(0, 0, SCREENWIDTH, SCREENHEIGHT);
-
 	
-		player.paint(g2);
-		// g2.dispose();
-		bs.show();
 	}
 
 	// Skickar vidare knappytryckningar till JPanel för behandling.
 	@Override
 	public void keyPressed(KeyEvent e) {
-		player.keyPressed(e);
+	player.keyPressed(e);
 		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) // Stänger av spelet om ESC
 													// trycks
 			System.exit(0);
@@ -155,7 +301,8 @@ public class Game extends Frame implements Runnable, KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		player.keyReleased(e);
+	player.keyReleased(e);
+		
 	}
 
 	@Override
--- Game/src/player/Player.java
+++ Game/src/player/Player.java
@@ -5,44 +5,59 @@ import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 
 public class Player {
-	private int X = 0;
-	private int Y = 0;
+	private int X = 0;	//Spelarens X-position
+	private int Y = 0;	//Spelarens Y-position
 	private int X_DIRECTION=0; // Variabel som säger åt vilket håll i x-led spelaren är på väg.
 	private int Y_DIRECTION=0; // Variabel som säger åt vilket håll i x-led spelaren är på väg.
-	private final int SPEED=2;
-	
+	private double YSPEED=2;	//Hastighet i Y-led
+	private double XSPEED=2;	//Hastighet i X-led
+	private final double GRAVITY=0.5;	//Konstant gravitation
+	private final double JUMPHEIGHT=-10;
 	public Player(){
 		
 	}
 	public void move(){
-		X += X_DIRECTION*SPEED;
-		Y += Y_DIRECTION*SPEED;
+		YSPEED+=GRAVITY;	//Ökar hastigheten i Y-led beroende på gravitation
+		X += X_DIRECTION*XSPEED;	//Förflyttar i X-led om knapp är nedtryckt
+		if(Y+YSPEED>500)	//Stannar vid "golvet" på 500 px
+		{
+			Y=501;
+			Y_DIRECTION=-1;	//Sätter direction så spelaren hoppar
+			YSPEED=0;	//Sätter hastigheten till noll
+		}
+		else
+			Y += YSPEED;	//Ökar position i Y-led
+		
+		
 	}
+	//Ritar ut spelaren
 	public void paint(Graphics2D g)
 	{
 		g.setColor(Color.RED);
 		g.fillRect(X, Y, 25, 25);
 	}
+	//Tar hand om knapptryckningar
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode()==KeyEvent.VK_D)
-			X_DIRECTION = 1;
-		else if (e.getKeyCode()==KeyEvent.VK_A)
-			X_DIRECTION = -1;
-		else if (e.getKeyCode()==KeyEvent.VK_W)
-			Y_DIRECTION = -1;
-		else if(e.getKeyCode()==KeyEvent.VK_S)
-			Y_DIRECTION = 1;
+			X_DIRECTION = 1;	// Sätter förflyttning i positiv riktning
+		if (e.getKeyCode()==KeyEvent.VK_A)
+			X_DIRECTION = -1;	// Sätter förflyttning i negativ riktning
+		if (e.getKeyCode()==KeyEvent.VK_W)
+		{
+			if(Y_DIRECTION == -1)
+				YSPEED = JUMPHEIGHT;	//spelaren hoppar
+			Y_DIRECTION = 0;	//Sätter Y_DIRECTION till 0 så spelaren inte kan hoppa förän han träffar backen igen.
+		}
+		
 		
 	}
 	public void keyReleased(KeyEvent e) {
 		if(e.getKeyCode()==KeyEvent.VK_D && X_DIRECTION == 1)
 			X_DIRECTION = 0;
-		else if (e.getKeyCode()==KeyEvent.VK_A && X_DIRECTION == -1)
-			X_DIRECTION = 0;
-		else if (e.getKeyCode()==KeyEvent.VK_W && Y_DIRECTION == -1)
-			Y_DIRECTION = 0;
-		else if(e.getKeyCode()==KeyEvent.VK_S && Y_DIRECTION == 1)
-			Y_DIRECTION = 0;
+		if (e.getKeyCode()==KeyEvent.VK_A && X_DIRECTION == -1)
+			X_DIRECTION = 0;	
+		if(e.getKeyCode()==KeyEvent.VK_W && YSPEED < 0)
+			YSPEED=0;	// Stannar gubben om hoppknappen släpps
 		
 	}
 }

dcfce5d7ea9ef768912ae22e3698b17d21d15468
Palsson123 <jacpa975@student.liu.se>
2013-05-03 23:59:52 +0200

    Commented in the class Images

--- Game/src/images/Images.java
+++ Game/src/images/Images.java
@@ -17,49 +17,58 @@ import javax.imageio.ImageIO;
 public class Images {
 	ArrayList<Image> allaBilder = new ArrayList<Image>();
 	ArrayList<String> allaBildNamn = new ArrayList<String>();
-	public Images(){
-		loadImages();
+	private final String loadPath = "./res/images"; // Mappen för bilder
+
+	public Images() {
+		loadImages(); // Laddar in alla bilder
 	}
-	public Image getImage(String imageName)
-	{
-		for(int i=0;i<allaBildNamn.size();i++)
-		{
-			
-			if(allaBildNamn.get(i).equals(imageName))
+
+	// Loopar igenom alla namn i allaBildNamn-arrayen och letar efter en bild
+	// med namnet angivet i imageName och skickar isåfall den bild med samma
+	// index från allaBilder
+	public Image getImage(String imageName) {
+		for (int i = 0; i < allaBildNamn.size(); i++) {
+			if (allaBildNamn.get(i).equals(imageName))
 				return allaBilder.get(i);
 		}
-		return null;
+		return null; // Om ingen bild hittats
 	}
-	private void loadImages(){
-		
-		try {
-		    Path startPath = Paths.get("./res/images");
-		    Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
-		        @Override
-		        public FileVisitResult preVisitDirectory(Path dir,
-		                BasicFileAttributes attrs) {
-		            return FileVisitResult.CONTINUE;
-		        }
 
-		        @Override
-		        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {    
-		            try {
+	// Denna funktion laddar in alla bilder i mappen loadPath
+	private void loadImages() {
+
+		try {
+			Path startPath = Paths.get(loadPath);
+			Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult preVisitDirectory(Path dir,
+						BasicFileAttributes attrs) {
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(Path file,
+						BasicFileAttributes attrs) {
+					try {
+						//Läser in bild och lägger in i array
 						allaBilder.add(ImageIO.read(new File(file.toString())));
-						allaBildNamn.add(file.toString().substring(file.toString().lastIndexOf("\\") + 1));
+						//Lägger till bildens namn till allBildNamn
+						allaBildNamn.add(file.toString().substring(
+								file.toString().lastIndexOf("\\") + 1));
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-		            return FileVisitResult.CONTINUE;
-		        }
+					return FileVisitResult.CONTINUE;
+				}
 
-		        @Override
-		        public FileVisitResult visitFileFailed(Path file, IOException e) {
-		            return FileVisitResult.CONTINUE;
-		        }
-		    });
+				@Override
+				public FileVisitResult visitFileFailed(Path file, IOException e) {
+					return FileVisitResult.CONTINUE;
+				}
+			});
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 }
